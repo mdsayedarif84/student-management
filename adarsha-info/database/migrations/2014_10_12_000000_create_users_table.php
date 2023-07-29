b966c692954b818df9fea9417903e0abda1d2c20
mdsayedarif84 <mdsayedarif84@gmail.com>
2023-07-29 09:17:24 +0600

    incomplete dashboard

--- adarsha-info/database/migrations/2014_10_12_000000_create_users_table.php
+++ adarsha-info/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role',['admin','user','teacher'])->default('user');
+            $table->enum('status',['active','inactive'])->default('active');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
